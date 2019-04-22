ba2c4ed01a045a80406b5fa2119976d8af617e82
Oleg Tagay <oleg.tagay@idt.net>
2019-04-22 15:49:02 +0300

    commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,9 +1,33 @@
 import org.junit.Assert;
+import org.junit.experimental.categories.Category;
 
 public class Test {
 
+    @Category(Regression.class)
     @org.junit.Test
     public void test(){
+        System.out.println("test");
+        Assert.assertTrue(true);
+    }
+
+    @Category(Regression.class)
+    @org.junit.Test
+    public void test1(){
+        System.out.println("test1");
+        Assert.assertTrue(true);
+    }
+
+    @Category(Performance.class)
+    @org.junit.Test
+    public void test2(){
+        System.out.println("test2");
+        Assert.assertTrue(true);
+    }
+
+    @Category(Performance.class)
+    @org.junit.Test
+    public void test3(){
+        System.out.println("test3");
         Assert.assertTrue(true);
     }
 }
